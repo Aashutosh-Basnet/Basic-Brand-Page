8601110312a7a400675fc9dc6f180055963f9c01
Aashutosh Basnet <aashutoshbasnet3344@gmail.com>
2024-06-25 22:43:49 +0530

    navbar section finished

---
+++ brand_page/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <p>NIKE</p>
+
+      <ul className="lists">
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+
+      <button className="login_button">LOGIN</button>
+    </div>
+  );
+};
+
+export default Navbar;
